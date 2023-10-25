7b651af2d9cb00be3eab5de7113bb12d9ed4c986
qwert ewr <ekonovalov9026@gmail.com>
2023-10-25 23:50:01 +0300

    dz1

---
+++ nodedz1.js
@@ -0,0 +1,27 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>
+{
+    if(req.url == '/'){
+        res.writeHead(200,{
+            'Content-Type':'text/html; charset=UTF-8',
+
+        });
+        res.end(`<a href = "/about">Другая страница</a><br /><h1>Количество загрузок = ${count1()}</h1>`);
+    }
+    else if(req.url =='/about') {
+        res.writeHead(200,{
+            'Content-Type':'text/html; charset=UTF-8',
+        });
+        res.end(`<a href = "/">Главная страница</a><br /><h1>Количество загрузок = ${count2()}</h1>`);
+    }
+});
+server.listen(3000);
+function counter(){
+    let n=1;
+     return function schet(){
+        return  n++;
+    }
+}
+let count1 = counter();
+let count2 = counter();
